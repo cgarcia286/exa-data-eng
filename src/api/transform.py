a747e0595c8cb9fda15a741024fc1456cb9f1942
Carlos Garcia <carlos.garcia@nextgenpty.com>
2023-09-02 13:48:06 +0100

    Added names and address models and its logic to store related data

--- src/api/transform.py
+++ src/api/transform.py
@@ -4,12 +4,14 @@ from uuid import UUID
 import json
 import os
 
+from fhir.resources.address import Address
+from fhir.resources.humanname import HumanName
 from fhir.resources.identifier import Identifier
 from fhir.resources.patient import Patient
 from sqlalchemy.orm import Session
 
 from config.logger_config import setup_logger
-from models.patient import PatientModel
+from models.patient import AddressModel, HumanNameModel, PatientModel
 
 LOGGER = setup_logger(__name__)
 
@@ -61,55 +63,134 @@ def get_identifiers(identifiers: List[Identifier]) -> dict:
 def transform_data(path: str, session: Session) -> list:
     """Transform patient's data"""
     LOGGER.info(f'Loading Data from {path} folder')
-    files = os.listdir(path)
+    # files = os.listdir(path)
     transformed_data = []
 
-    with open(os.path.join(path, files[0]), 'r') as json_file:
-        fhir_data = json.load(json_file)
+    for filename in os.listdir(path):
+        if filename.endswith(".json"):
+            with open(os.path.join(path, filename), "r") as json_file:
+                fhir_data = json.load(json_file)
 
-        for entry in fhir_data.get('entry', []):
-            resource = entry.get('resource', {})
+    # with open(os.path.join(path, files[0]), 'r') as json_file:
+    #     fhir_data = json.load(json_file)
 
-            resource_type = resource.get('resourceType')
-            if resource_type == 'Patient':
-                patient = Patient.parse_obj(resource)
+            for entry in fhir_data.get('entry', []):
+                resource = entry.get('resource', {})
 
-                identifiers = get_identifiers(patient.identifier)
-                social_security_number = identifiers['social_security_number']
-                drivers_license = identifiers['drivers_license']
-                passport_number = identifiers['passport_number']
+                resource_type = resource.get('resourceType')
+                if resource_type == 'Patient':
+                    patient = Patient.parse_obj(resource)
 
-                # This resource is generally assumed to be active if no value
-                # provided for the active element based on official FHIR docs
-                active = patient.active if patient.active else True
+                    # Es una lista!
+                    # language = patient.communication[0].language.text
+                    identifiers = get_identifiers(patient.identifier)
+                    social_security_number = identifiers['social_security_number']
+                    drivers_license = identifiers['drivers_license']
+                    passport_number = identifiers['passport_number']
 
-                patient_data = {
-                    'id': UUID(patient.id),
-                    'active': active,
-                    'gender': patient.gender,
-                    'social_security_number': social_security_number,
-                    'drivers_license': drivers_license,
-                    'passport_number': passport_number,
-                    'phone_number': patient.telecom[0].value,
-                    'marital_status': patient.maritalStatus.text
-                }
+                    # This resource is generally assumed to be active if no value is
+                    # provided for the active element based on official FHIR docs
+                    active = patient.active if patient.active else True
 
-                obj_exists, existing_patient = item_exists(
-                    PatientModel,
-                    patient.id,
-                    session
-                )
+                    patient_data = {
+                        'id': UUID(patient.id),
+                        'active': active,
+                        'gender': patient.gender,
+                        'social_security_number': social_security_number,
+                        'drivers_license': drivers_license,
+                        'passport_number': passport_number,
+                        'phone_number': patient.telecom[0].value,
+                        'marital_status': patient.maritalStatus.text
+                    }
 
-                if obj_exists:
-                    updated_data = compare_data(existing_patient, patient_data)
-                    if updated_data:
-                        # Update fields that have changed only
-                        for field, value in updated_data.items():
-                            setattr(existing_patient, field, value)
-                        LOGGER.info(f'Updating patient: {patient.id}')
-                        session.commit()
-                else:
-                    LOGGER.info(f'Adding new patient {patient.id}')
-                    transformed_data.append(PatientModel(**patient_data))
+                    obj_exists, existing_patient = item_exists(PatientModel, patient.id, session)
+                    if obj_exists:
+                        updated_data = compare_data(existing_patient, patient_data)
+                        if updated_data:
+                            # Update fields that have changed only
+                            for field, value in updated_data.items():
+                                setattr(existing_patient, field, value)
+                            LOGGER.info(f'Updating patient: {patient.id}')
+                            session.commit()
+                    else:
+                        LOGGER.info(f'Adding new patient {patient.id}')
+                        transformed_data.append(PatientModel(**patient_data))
+
+                    names: List[HumanName] = patient.name
+                    for name in names:
+                        # This elements can have multiple values or to be null
+                        given = ' '.join(name.given) if name.given else None
+                        prefix = ' '.join(name.prefix) if name.prefix else None
+                        suffix = ' '.join(name.suffix) if name.suffix else None
+
+                        name_data = {
+                            'patient_id': UUID(patient.id),
+                            'use': name.use,
+                            'given': given,
+                            'family': name.family,
+                            'prefix': prefix,
+                            'suffix': suffix
+                        }
+
+                        existing_names = session.query(
+                            HumanNameModel
+                        ).filter_by(patient_id=patient.id).all()
+
+                        name_exists = any(
+                            n.family == name_data['family'] and
+                            n.given == name_data['given'] and
+                            n.use.value == name_data['use']
+                            for n in existing_names
+                        )
+
+                        if not name_exists:
+                            LOGGER.info(f'Adding new name for patient {patient.id}')
+                            transformed_data.append(HumanNameModel(**name_data))
+                        else:
+                            # Update name only if given or family name changed
+                            name_to_update = session.query(HumanNameModel).filter_by(
+                                patient_id=patient.id,
+                                given=name_data['given'],
+                                family=name_data['family']
+                            ).first()
+                            name_to_update.use = name_to_update.use.value
+
+                            updated_fields = compare_data(name_to_update, name_data)
+                            if updated_fields:
+                                for field, value in updated_fields.items():
+                                    setattr(name_to_update, field, value)
+                                LOGGER.info(f'Updating name: {name_to_update.id}')
+                                session.commit()
+
+                    addresses: List[Address] = patient.address
+                    for address in addresses:
+                        address_data = {
+                            'patient_id': UUID(patient.id),
+                            'use': address.use,
+                            'type': address.type,
+                            'line': ' '.join(address.line),
+                            'city': address.city,
+                            'district': address.district,
+                            'state': address.state,
+                            'postal_code': address.postalCode,
+                            'country': address.country
+                        }
+
+                        existing_addresses = session.query(
+                            AddressModel
+                        ).filter_by(patient_id=patient.id).all()
+
+                        address_exists = any(
+                            a.line == address_data['line'] and
+                            a.city == address_data['city'] and
+                            a.use == address_data['use']
+                            for a in existing_addresses
+                        )
+
+                        if not address_exists:
+                            LOGGER.info(f'Adding new address for patient {patient.id}')
+                            transformed_data.append(AddressModel(**address_data))
+
+                        # TODO: Implementation for Update on change
 
     return transformed_data
--- src/models/patient.py
+++ src/models/patient.py
@@ -1,9 +1,39 @@
 """Model for Patient table"""
 from uuid import uuid4
 
-from sqlalchemy import Boolean, Column, String, UUID
+from enum import Enum as PythonEnum
+from sqlalchemy import Boolean, Column, Enum, ForeignKey, String, UUID
+from sqlalchemy.orm import relationship
 
 from .base import TimeStampModel
+
+
+class HumanNameUse(PythonEnum):
+    usual = 'usual'
+    official = 'official'
+    temp = 'temp'
+    nickname = 'nickname'
+    anonymous = 'anonymous'
+    old = 'old'
+    maiden = 'maiden'
+
+
+class HumanNameModel(TimeStampModel):
+    __tablename__ = 'human_names'
+
+    id = Column(UUID, primary_key=True, index=True, default=uuid4)
+    patient_id = Column(UUID, ForeignKey('patients.id'))
+    use = Column(Enum(HumanNameUse))
+    given = Column(String, index=True)
+    family = Column(String, index=True)
+    prefix = Column(String)
+    suffix = Column(String)
+
+    # Establish the inverse relationship with the Patients table
+    patient = relationship('PatientModel', back_populates='names')
+
+    def __repr__(self):
+        return f'{self.given} {self.family}'
 
 
 class PatientModel(TimeStampModel):
@@ -18,6 +48,41 @@ class PatientModel(TimeStampModel):
     phone_number = Column(String)
     marital_status = Column(String)
 
+    # One-to-many relationship with patient name table
+    names = relationship('HumanNameModel', back_populates='patient')
+
+    # One-to-many relationship with patient address table
+    adresses = relationship('AddressModel', back_populates='patient')
+
     def __repr__(self):
         full_name = f'{self.names[0]}' if self.names else 'Unknown'
         return f'Patient(id={self.id}, full_name={full_name})'
+
+
+class AddressUse(PythonEnum):
+    home = 'home'
+    work = 'work'
+    temp = 'temp'
+    old = 'old'
+    billing = 'billing'
+
+
+class AddressModel(TimeStampModel):
+    __tablename__ = 'adresses'
+
+    id = Column(UUID, primary_key=True, index=True, default=uuid4)
+    patient_id = Column(UUID, ForeignKey('patients.id'))
+    use = Column(String)
+    type = Column(String)
+    line = Column(String)
+    city = Column(String)
+    district = Column(String)
+    state = Column(String)
+    postal_code = Column(String)
+    country = Column(String)
+
+    # Establish the inverse relationship with the Patients table
+    patient = relationship('PatientModel', back_populates='adresses')
+
+    def __repr__(self):
+        return f'Address(id={self.id})'
